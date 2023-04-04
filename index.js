374336ea292b49f625a04ce35ca65ed68f4a4981
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-04-04 21:46:15 +0200

    Comments geplaatst

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-import express from 'express'
+import express from "express";
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/'
+const url = "https://whois.fdnd.nl/api/v1/squad/";
 
 // Maak een nieuwe express app
-const app = express()
+const app = express();
 
 // Stel in hoe we express gebruiken
-app.set('view engine', 'ejs')
-app.set('views', './views')
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', (request, response) => {
-  console.log(request.query.squad)
+app.get("/", (request, response) => {
+  console.log(request.query.squad);
 
-  let slug = request.query.squad || 'squad-b-2022'
-  let orderBy = request.query.orderBy || 'surname'
-  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+  // Maak variabelen aan
+  let slug = request.query.squad || "squad-b-2022";
+  let orderBy = request.query.orderBy || "surname";
+  let squadUrl = url + slug + "?orderBy=" + orderBy + "&direction=ASC";
 
   fetchJson(squadUrl).then((data) => {
-    response.render('index', data)
-  }) 
-})
-
-// app.get('/members', (request, response) => {
-//   response.send('Joepie!!')
-// })
+    response.render("index", data);
+  });
+});
 
 // Stel het poortnummer in en start express
-app.set('port', process.env.PORT || 8000)
-app.listen(app.get('port'), function () {
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.set("port", process.env.PORT || 8000);
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
 
 /**
  * Wraps the fetch api and returns the response body parsed through json
@@ -41,5 +38,5 @@ app.listen(app.get('port'), function () {
 async function fetchJson(url) {
   return await fetch(url)
     .then((response) => response.json())
-    .catch((error) => error)
+    .catch((error) => error);
 }
